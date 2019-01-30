eac83b728c358528d4188b75b035f39682881f28
richard.supak Supak <richard.supak@icloud.com>
2019-01-30 04:36:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,11 @@
 # code your #valid_move? method here
-
+def valid_move?(board, index)
+  return false if index > board.length - 1
+  position_taken?(board, index)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  return false if board[index] == '' or board.index == ' '
+  return false if board[index].nil?
+end
